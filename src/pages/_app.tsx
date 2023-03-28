e8ffb2e5293df8d2f7af4660e4c42160bbaed6ac
pavels <dev.pavels.garklavs@gmail.com>
2023-03-28 22:04:48 +0300

    fix?

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,11 @@
+import { type AppType } from "next/app";
 import { ClerkProvider } from "@clerk/nextjs";
 
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
 
-const MyApp = ({ Component, pageProps }) => {
+const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ClerkProvider {...pageProps} >
       <Component {...pageProps} />
